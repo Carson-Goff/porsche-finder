c6052621c825749d8d101228dee0a4173d6dd43f
Carson <carsonbgoff@gmail.com>
2024-02-20 13:51:20 -0500

    fixed json capture and started db construction

--- components/dataCollector/src/main/scrapeBat.py
+++ components/dataCollector/src/main/scrapeBat.py
@@ -5,6 +5,7 @@ import configparser
 import os
 import logging
 import json
+import re
 
 class dataCollector:
     def __init__(self, ini_file, log_file):
@@ -30,26 +31,49 @@ class dataCollector:
 
     def parse_listings(self, soup, new=True):
         self.logger.info('Parsing listings')
+        extracted_data = []
+
         if new:
             listings = soup.find_all('div', class_='listing-card listing-card-separate')
+            for listing in listings:
+                data = {
+                    'listing_id': listing.get('data-listing_id', None),
+                    'title': listing.find('h3').get_text(strip=True),
+                    'url': listing.find('h3').find('a')['href'],
+                    'image_url': listing.find('img')['src'] if listing.find('img') else None,
+                    'price': listing.find('span', class_='bid-formatted').get_text(strip=True) if listing.find('span', class_='bid-formatted') else None,
+                    'sold': None
+                }
+                extracted_data.append(data)
         else:
             script = soup.find('script', {'id': 'bat-theme-auctions-completed-initial-data'})
-            script_content = script.string
-            json_str = script_content.split('var auctionsCompletedInitialData = ')[1].split('/* ]]> */')[0].strip()
-            listings = json.loads(json_str)['items']
-
-        extracted_data = []
-        for listing in listings:
-            data = {
-                'listing_id': listing.get('data-listing_id', None),
-                'title': listing.find('h3').get_text(strip=True),
-                'url': listing.find('h3').find('a')['href'],
-                'image_url': listing.find('img')['src'] if listing.find('img') else None,
-                'price': listing.find('span', class_='bid-formatted').get_text(strip=True) if listing.find('span', class_='bid-formatted') else None
-            }
-            extracted_data.append(data)
+            if script and script.string:
+                script_content = script.string
+                json_start = script_content.find('{')
+                json_end = script_content.rfind('}') + 1
+                json_str = script_content[json_start:json_end]
+                try:
+                    data = json.loads(json_str)
+                    listings = data.get('items', [])
+                    for listing in listings:
+                        sold = False
+                        if 'Sold' in listing.get('sold_text', None):
+                            sold = True
+                        data = {
+                            'listing_id': listing.get('id', None),
+                            'title': listing.get('title', None),
+                            'url': listing.get('url', None),
+                            'image_url': listing.get('thumbnail_url', None),
+                            'price': listing.get('current_bid_formatted', None),
+                            'sold': sold
+                        }
+                        extracted_data.append(data)
+                except json.JSONDecodeError as e:
+                    self.logger.error(f'JSON decoding error: {e}')
+                    return []
 
         return extracted_data
+
 
     def log_listings(self, listings):
         self.logger.info('Logging listings')
@@ -60,13 +84,15 @@ class dataCollector:
 
     def run(self):
         soup = self.fetch_listings()
-        new_listings = self.parse_listings(soup)
-        print(*new_listings, sep='\n')
+        # new_listings = self.parse_listings(soup)
+        old_listings = self.parse_listings(soup, False)
+        # print(*new_listings, sep='\n')
+        print(*old_listings, sep='\n')
 
-        if new_listings:
-            self.log_listings(new_listings)
-        else:
-            print("No new listings found.")
+        # if new_listings:
+        #     self.log_listings(new_listings)
+        # else:
+        #     print("No new listings found.")
 
 if __name__ == '__main__':
     ini_file = 'porsche-finder.ini'
